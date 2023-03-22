f20fca5a2ad2c9466ae79075b15abe4114125b38
Karolina Dyjak <karolina.dyjak5@gmail.com>
2023-03-22 12:24:03 +0100

    bug fixing - switching between activities

--- app/src/main/java/com/apartments/gimo/IntroActivity.kt
+++ app/src/main/java/com/apartments/gimo/IntroActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class IntroActivity : AppCompatActivity() {
 
-    private val SPLASH_DISPLAY_LENGTH: Long = 2500
-    
+//    private val SPLASH_DISPLAY_LENGTH: Long = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        Handler(mainLooper).postDelayed({
-            startActivity(Intent(this, StartActivity::class.java))
-            finish()
-        }, SPLASH_DISPLAY_LENGTH)
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, StartActivity::class.java))
+//            finish()
+//        }, 2500.toLong())
     }
 }
--- app/src/main/java/com/apartments/gimo/StartActivity.kt
+++ app/src/main/java/com/apartments/gimo/StartActivity.kt
@@ -1,13 +1,20 @@
 package com.apartments.gimo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500.toLong())
 
     }
 }
